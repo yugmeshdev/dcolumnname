98acf598f68695aff25c1cd65869bc74a31087c8
yugmesh Sonawane <yugmeshk2139@gmail.com>
2024-03-24 11:56:22 +0530

    startup code

---
+++ src/main/java/com/springusercase/dcolumname/DcolumnameApplication.java
@@ -0,0 +1,13 @@
+package com.springusercase.dcolumname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcolumnameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcolumnameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springusercase/dcolumname/DcolumnameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springusercase.dcolumname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcolumnameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
